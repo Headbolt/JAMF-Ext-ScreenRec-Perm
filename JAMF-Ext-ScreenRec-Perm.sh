1b2ddfd1797740dfe71a03cd2fee87e6fa79408b
Headbolt <54707369+Headbolt@users.noreply.github.com>
2020-01-10 14:53:17 +0000

    Update JAMF-Ext-ScreenRec-Perm.sh

--- JAMF-Ext-ScreenRec-Perm.sh
+++ JAMF-Ext-ScreenRec-Perm.sh
@@ -18,17 +18,25 @@
 #
 # HISTORY
 #
-#   Version: 1.1 - 09/01/2020
+#   Version: 1.2 - 10/01/2020
 #
 #   - 06/01/2020 - V1.0 - Created by Headbolt
 #   - 09/01/2020 - V1.1 - Updated by Headbolt
 #							More thorough error checking and notation
+#   - 10/01/2020 - V1.2 - Updated by Headbolt
+#							Now allows for permissions issues when no user logged in
+#								Success now writes the correct value to a PLIST
+#								On execution the Value is read in, and if no user is logged in,
+#								resulting in and error, the read in Value is Output to keep from
+#								having Error values or blank entries written
 #
 ###############################################################################################################################################
 #
 # DEFINE VARIABLES & READ IN PARAMETERS
 #
 ###############################################################################################################################################
+#
+User=$(stat -f '%Su' /dev/console) # Grab Current Console User
 #
 osMajor=$( /usr/bin/sw_vers -productVersion | /usr/bin/awk -F. '{print $1}' )
 osMinor=$( /usr/bin/sw_vers -productVersion | /usr/bin/awk -F. '{print $2}' )
@@ -37,6 +45,8 @@ osPatch=$( /usr/bin/sw_vers -productVersion | /usr/bin/awk -F. '{print $3}' )
 AppIDstring=ScreenConnect # Grab the identifier to use when searching the TCC Database from JAMF variable #4 eg ScreenConnect
 # Note : this can usually be found by manually allowing on a test machine and then running the below command
 # sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access'
+#
+CurrentAppPerms=$(defaults read /var/JAMF/ScreenRecording-Perms.plist $AppIDstring 2>&1) # Grab last written value
 #
 ###############################################################################################################################################
 #
@@ -63,32 +73,40 @@ fi
 #
 if [[ "$CATplus" == "YES" ]]
 	then
-		App=$(sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access' | grep -i $AppIDstring) # Find the line for the App
-		AccErr=$(sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access' 2>&1 | grep unable) # Check for permissions error
-		read -ra AppStatusArray <<< "$App" # Read In the Array
-		#
-		IFS='|' # Internal Field Seperator Delimiter is set to Pipe (|)
-		AppStatus=$(echo $AppStatusArray | awk '{ print $4 }')
-		unset IFS
-		#
-		if [[ "$AccErr" == "" ]] # Check if there was a permissions error accessing the TCC.db file
-			then 
-				if [[ $AppStatus == 1 ]] # Check if the app has Screen Recording permission enabled
-					then
-						RESULT="SET"
-					else
-						if [[ $AppStatus == "" ]]
+		if [[ $User != "root" ]] # Check if a user is logged in, if not this can result in errors
+			then
+				App=$(sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access' | grep -i $AppIDstring) # Find the line for the App
+				AccErr=$(sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db 'select * from access' 2>&1 | grep unable) # Check for permissions error
+				read -ra AppStatusArray <<< "$App" # Read In the Array
+				#
+				IFS='|' # Internal Field Seperator Delimiter is set to Pipe (|)
+				AppStatus=$(echo $AppStatusArray | awk '{ print $4 }')
+				unset IFS
+				#
+				if [[ "$AccErr" == "" ]] # Check if there was a permissions error accessing the TCC.db file
+					then 
+						if [[ $AppStatus == 1 ]] # Check if the app has Screen Recording permission enabled
 							then
-								RESULT="NOT PRESENT"
+								RESULT="SET"
 							else
-								RESULT="NOT SET"
+								if [[ $AppStatus == "" ]]
+									then
+										RESULT="NOT PRESENT"
+									else
+										RESULT="NOT SET"
+								fi
 						fi
+					else 
+						RESULT="PERMISSIONS ERROR"
 				fi
-			else 
-				RESULT="PERMISSIONS ERROR"
-			fi
+				#
+				/bin/echo "<result>$RESULT</result>" # Write Result out
+				defaults write /var/JAMF/ScreenRecording-Perms.plist $AppIDstring -string "$RESULT" # Write Result into PLIST
+				#
+			else
+				/bin/echo "<result>$CurrentAppPerms</result>" # If no User logged in, re-write last known value
+		fi
 	else
 		RESULT="OS is Lower than Catalina"
+		/bin/echo "<result>$RESULT</result>" # Write Result out
 fi
-#
-/bin/echo "<result>$RESULT</result>"
